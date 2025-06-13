a36d1f486d26a9a188bc916cd5da5ebd73b07e6f
Donovan Harrison <donjuan0802@gmail.com>
2025-06-12 21:29:29 -0500

    Add rate limiting functionality with RateLimitingFilter and register it in WeatherApiApplication

--- src/main/java/io/github/dhar135/WeatherApi/WeatherApiApplication.java
+++ src/main/java/io/github/dhar135/WeatherApi/WeatherApiApplication.java
@@ -2,7 +2,11 @@ package io.github.dhar135.WeatherApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+
+import io.github.dhar135.WeatherApi.config.RateLimitingFilter;
 
 @EnableCaching
 @SpringBootApplication
@@ -11,6 +15,14 @@ public class WeatherApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApiApplication.class, args);
 
+	}
+
+	@Bean
+	public FilterRegistrationBean<RateLimitingFilter> rateLimitingFilter() {
+		FilterRegistrationBean<RateLimitingFilter> registration = new FilterRegistrationBean<>();
+		registration.setFilter(new RateLimitingFilter());
+		registration.addUrlPatterns("/api/weather/*");
+		return registration;
 	}
 
 }
